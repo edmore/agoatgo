cb42c32988da4d887b97c7b6cfbbe34749dda708
Edmore Moyo <etmoyo@gmail.com>
2013-02-28 00:05:20 +0200

    getting started

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "math"
+
+func add1(i int) int {
+	return i + 1
+}
+
+func pi() float32 {
+	return math.Pi
+}
+
+func languages() string {
+	lang := "Ruby"
+	return lang
+}
+func main() {
+	fmt.Println("Add 1 : ", add1(2))
+	fmt.Println("Value of Pi : ", pi())
+	fmt.Println("Fav OO language : ", languages())
+}
