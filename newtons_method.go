c137833b74e39045556ee341dd2e9cd0bb6e03ab
Edmore Moyo <etmoyo@gmail.com>
2013-03-09 00:28:58 +0200

    declaration(:=), assignment(=)

---
+++ newtons_method.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func Sqrt(x float64) float64 {
+    guess := x/2.0
+
+    for y:=0; y < 11; y++ {
+        guess = (guess + x/guess)/2
+    }
+    return guess
+}
+
+func main() {
+    fmt.Println(Sqrt(36))
+}
