f330abe8ac5651498802a50105d78000e1c4c596
Edmore Moyo <etmoyo@gmail.com>
2013-03-09 00:28:34 +0200

    more learning

--- hello.go
+++ hello.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func add1(i int) int {
 	return i + 1
@@ -16,8 +18,21 @@ func languages() string {
 	return lang
 }
 
+func looper(max int) int {
+	sum := 0
+	for i := 0; i <= max; i++ {
+		if i != 1 {
+			sum += i
+		}
+	}
+	return sum
+}
+
 func main() {
 	fmt.Println("Add 1 : ", add1(2))
 	fmt.Println("Value of Pi : ", pi())
 	fmt.Println("Fav OO language : ", languages())
+	fmt.Println("Sum is : ", looper(3))
 }
+
+
