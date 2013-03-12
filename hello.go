981285036ac8433cb0c20fdeea1f89763c4380fd
Edmore Moyo <etmoyo@gmail.com>
2013-03-12 20:39:20 +0200

    maps and map literals

--- hello.go
+++ hello.go
@@ -34,16 +34,19 @@ type Person struct {
 }
 
 func main() {
-
 	p := Person{Name : "Edmore"}
         q := new(Person)
 	q.Name = "Tu"
 	r := &p
         r.Name = "Tu"
-	fmt.Println("Add 1 : ", add1(2))
+	s := []int{2,4,5,6,7}
+
+   	fmt.Println("Add 1 : ", add1(2))
 	fmt.Println("Value of Pi : ", pi())
 	fmt.Println("Fav OO language : ", languages())
 	fmt.Println("Sum is : ", looper(3))
 	fmt.Println("Look Ma a struct changed by a pointer : ", p.Name)
 	fmt.Println("Look Ma using the new keyword: ", q.Name)
+	fmt.Println("s: ", s[1:4])
+
 }
