6490a0a406f3e9c2ae05cfd2145e98a45b5497e3
Edmore Moyo <etmoyo@gmail.com>
2013-03-17 06:06:46 +0200

    Go is a lot of fun

--- hello.go
+++ hello.go
@@ -4,7 +4,21 @@ import (
 	"fmt"
 	"math"
 	"strings"
+	"time"
 )
+
+type Person struct {
+  Name string
+  Surname string
+}
+
+func (p *Person) setSurname(s string){
+	p.Surname = s
+}
+
+func (p *Person) getName() string {
+	return p.Name
+}
 
 func add1(i int) int {
 	return i + 1
@@ -29,11 +43,6 @@ func looper(max int) int {
 	return sum
 }
 
-type Person struct {
-  Name string
-  Surname string
-}
-
 func WordCount(s string) map[string]int {
 	m := make(map[string]int)
 	split := strings.Fields(s)
@@ -44,7 +53,6 @@ func WordCount(s string) map[string]int {
 	return m
 }
 
-
 func fibonacci() func() int {
 	x, y, z := 0, 1, 0
 	return func() int {
@@ -53,13 +61,20 @@ func fibonacci() func() int {
 	}
 }
 
+func day_of_week() time.Weekday {
+	return time.Now().Weekday()
+}
+
 func main() {
 	p := Person{Name : "Edmore"}
         q := new(Person)
 	q.Name = "Tu"
 	r := &p
         r.Name = "Tu"
+	r.setSurname("Moyo2")
+
 	s := []int{2,4,5,6,7}
+
         m := make(map[string]int)
         m["Age"] = 30
 
@@ -72,7 +87,9 @@ func main() {
 	fmt.Println("s: ", s[1:4])
 	fmt.Println("Age : ", m["Age"])
         fmt.Println(WordCount("I ate a donut. Then I ate another donut."))
-
+        fmt.Println(day_of_week())
+	fmt.Println(r)
+	fmt.Println(r.getName())
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
 		fmt.Println(f())
