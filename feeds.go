2908ed79c1338372926b65de6000c215b3125097
Edmore Moyo <etmoyo@gmail.com>
2013-03-25 20:19:02 +0200

    changes to feeds to add dont care variables

--- feeds.go
+++ feeds.go
@@ -12,7 +12,8 @@ func get_venue_key(v string, attribute string) string {
 }
 
 func main() {
-	//app_root := "/usr/local/camera_dashboard"
+	app_root := "/usr/local/camera_dashboard"
+	fmt.Println(app_root)
 
         c, err := redis.Dial("tcp", ":6379")
         defer c.Close()
@@ -21,18 +22,22 @@ func main() {
 		fmt.Println(err)
 	}
 
-	venue_list, err := redis.Strings(c.Do("LRANGE", "venues", 0, -1))
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
+	venue_list, _ := redis.Strings(c.Do("LRANGE", "venues", 0, -1))
 	messages := make(chan string)
-	for _, v := range venue_list {
-		venue_name := get_venue_key(v, ":venue_name")
 
+	for _, v := range venue_list {
+		venue_name, _ := redis.String(c.Do("GET", get_venue_key(v, ":venue_name")))
+		cam_url, _ := redis.String(c.Do("GET", get_venue_key(v, ":cam_url")))
+		cam_user, _ := redis.String(c.Do("GET", get_venue_key(v, ":cam_user")))
+		cam_password, _ := redis.String(c.Do("GET", get_venue_key(v, ":cam_password")))
+
+                login_cridentials := ""
+		if (cam_user != ""){
+			login_cridentials = strings.Join([]string{"-u ", cam_password}, "")
+		}
+		fmt.Println(login_cridentials, "", cam_url)
 		go func(){
-			fmt.Println("Running feed for", venue_name)
+			fmt.Println("Running feed for", venue_name, "...")
 			messages <- "done"
 		}()
 		fmt.Println(<-messages)
